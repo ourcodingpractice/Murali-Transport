e93c7d86900f321336d6bebe1cf1dc244f4792d6
Anusha <anusha@digitaltaas.com>
2021-10-05 12:45:19 -0700

    Register vehicle api end point changes

--- client/src/app/api/vehicles-api.jsx
+++ client/src/app/api/vehicles-api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:8080/vehicles/post';
+const url = 'https://muralitransport.wl.r.appspot.com/vehicles/post';
 const postVehicle = (data) => {
     return axios.post(url, data);
 }
